02224d2efc6ff86b0c52365639c7c628ce86c312
jeonchangbae <avantgard7@gmail.com>
2022-02-22 08:35:55 +0000

    TodoApp onRemove event test ok

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -15,7 +15,9 @@ const TodoApp = () =>{
             done : true
         }
     ]);
+    
     const nextId = useRef(3); // 새로 추가 할 항목에서 사용할 id
+    
     const onInsert =useCallback(
         text => {
             setTodos(
@@ -28,16 +30,23 @@ const TodoApp = () =>{
             nextId.current += 1;
         },[todos]
     );
+    
     const onToggle = useCallback(
         id => {
             setTodos(todos.map((todo) => 
             todo.id === id ? {...todo, done: !todo.done} : todo));
         },[todos]
-    )
+    );
+    
+    const onRemove = useCallback(
+        id => {
+            setTodos(todos.filter(todo => todo.id !== id));
+        },[todos]
+    );
     return(
     <div>
         <TodoForm data-testid="helloworld" onInsert={onInsert}/>
-        <TodoList todos={todos} onToggle={onToggle}/>
+        <TodoList todos={todos} onToggle={onToggle} onRemove={onRemove}/>
     </div>
     ); 
 }
