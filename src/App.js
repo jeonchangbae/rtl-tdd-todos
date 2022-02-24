8aa27e590c385c53601a3f30b28173c0d324b248
jeonchangbae <avantgard7@gmail.com>
2022-02-24 01:22:46 +0000

    jenkins with github test2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import TodoApp from './TodoApp'
 function App() {
   return (
     <div className="App">
-      <h2>jenkins with github</h2>
+      <h2>jenkins with github test</h2>
       <TodoApp />
       learn react
     </div>
